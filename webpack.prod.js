420654c9b9191156f3bef8111d3bacb50405a24e
2goodAP <45424904+2goodAP@users.noreply.github.com>
2019-05-06 06:01:36 +0545

    Changed methods of building and testing the app
    * Added 3 webpack conifguration files
    * Added ability to 'npm start' and 'npm run build'

---
+++ webpack.prod.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: '[name].[contentHash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'src/boilerplate.html',
+                minify: {
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    removeScriptTypeAttributes: true,
+                    removeStyleLinkTypeAttributes: true
+                }
+            })
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_module/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // 2nd
+                    'css-loader' // 1st
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, // 3rd
+                    'css-loader', // 2nd
+                    'scss-loader' // 1st
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contentHash].css'
+        })
+    ]
+})
