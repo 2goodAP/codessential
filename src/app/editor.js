3f47c635feda38f810bd8222faf0dde32f60ccd8
2goodAP <45424904+2goodAP@users.noreply.github.com>
2019-05-26 17:56:41 +0545

    Adds useful additional features to the text editor
    This commit adds the following features to the editor:
    + Monokai dark theme
    + Syntax highlighting
    + Code completion with <Ctrl-Space>

--- src/app/editor.js
+++ src/app/editor.js
@@ -1,5 +1,11 @@
 const CodeMirror = require('codemirror');
+import '../../node_modules/codemirror/lib/codemirror.css';
+import '../../node_modules/codemirror/addon/hint/show-hint.css';
 import '../../node_modules/codemirror/mode/javascript/javascript';
+import '../../node_modules/codemirror/addon/hint/show-hint';
+import '../../node_modules/codemirror/addon/hint/javascript-hint';
+import '../../node_modules/codemirror/addon/edit/closebrackets';
+import '../../node_modules/codemirror/addon/edit/matchbrackets';
 import '../css/main.css';
 
 let codeditor = document.getElementById('codeditor');
@@ -7,5 +13,11 @@ let codeditor = document.getElementById('codeditor');
 export let editor = CodeMirror.fromTextArea(codeditor, {
     lineNumbers: true,
     mode: 'javascript',
-    theme: 'monokai'
+    theme: 'monokai',
+    extraKeys: {
+        'Ctrl-Space': 'autocomplete'
+    },
+    matchBrackets: true,
+    autoCloseBrackets: true
 });
+
